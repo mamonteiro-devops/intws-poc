02fcf6203b40635653854bece8024ded0b2190c9
Manuel Monteiro <manuel.monteiro@kuehne-nagel.com>
2023-07-17 22:45:30 +0100

    NOISSUE - main update

--- steel-eye-nginx-ingress-controller/ingress/k8s-ingress.tf
+++ steel-eye-nginx-ingress-controller/ingress/k8s-ingress.tf
@@ -1,39 +1,3 @@
-#resource "kubernetes_ingress_v1" "ingress" {
-#  wait_for_load_balancer = true
-#  metadata {
-#    name = "simple-steel-eye-ingress"
-#  }
-#  spec {
-#    ingress_class_name = "nginx"
-#    rule {
-#      http {
-#        path {
-#          backend {
-#            service {
-#              name = "simple-steel-eye-app1-service"
-#              port {
-#                number = 5678
-#              }
-#            }
-#          }
-#          path = "/app1"
-#        }
-#        path {
-#          backend {
-#            service {
-#              name = "simple-steel-eye-app2-service"
-#              port {
-#                number = 5678
-#              }
-#            }
-#          }
-#          path = "/app2"
-#        }
-#      }
-#    }
-#  }
-#}
-
 resource "kubernetes_ingress_v1" "ingress-deployment" {
   wait_for_load_balancer = true
   metadata {
