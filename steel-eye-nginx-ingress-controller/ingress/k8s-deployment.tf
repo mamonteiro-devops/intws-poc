eb0155db14eca6016deab75e33fbcfc9e8d8b89b
Manuel Monteiro <manuel.monteiro@kuehne-nagel.com>
2023-07-24 23:17:20 +0100

    NOISSUE

--- steel-eye-nginx-ingress-controller/ingress/k8s-deployment.tf
+++ steel-eye-nginx-ingress-controller/ingress/k8s-deployment.tf
@@ -30,7 +30,39 @@ resource "kubernetes_deployment" "example_deployment_lb" {
   }
 }
 
-resource "kubernetes_deployment" "example_deployment_ing" {
+
+resource "kubernetes_deployment" "example_deployment_ing_app1" {
+  metadata {
+    name = "deployment-ing"
+  }
+
+  spec {
+    replicas = 4
+
+    selector {
+      match_labels = {
+        app = "steel-eye-app-ing"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "steel-eye-app-ing"
+        }
+      }
+
+      spec {
+        container {
+          image = "hashicorp/http-echo"
+          name  = "simple-steel-eye-app1"
+          args  = ["-text=Hello from my simple-steel-eye-app1"]
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_deployment" "example_deployment_ing_app2" {
   metadata {
     name = "deployment-ing"
   }
--- steel-eye-nginx-ingress-controller/ingress/k8s-services.tf
+++ steel-eye-nginx-ingress-controller/ingress/k8s-services.tf
@@ -6,7 +6,7 @@ resource "kubernetes_service_v1" "app1_service-ing" {
     #The "selector" field is used to select the Pods that the Service will route traffic to.
     #In this case, the value of the "app" label in the Deployment's selector is used.
     selector = {
-      app = kubernetes_deployment.example_deployment_ing.spec.0.selector.0.match_labels.app
+      app = kubernetes_deployment.example_deployment_ing_app1.spec.0.selector.0.match_labels.app
     }
     port {
       port = 5678
@@ -22,7 +22,7 @@ resource "kubernetes_service_v1" "app2_service-ing" {
     #The "selector" field is used to select the Pods that the Service will route traffic to.
     #In this case, the value of the "app" label in the Deployment's selector is used.
     selector = {
-      app = kubernetes_deployment.example_deployment_ing.spec.0.selector.0.match_labels.app
+      app = kubernetes_deployment.example_deployment_ing_app2.spec.0.selector.0.match_labels.app
     }
     port {
       port = 5678
