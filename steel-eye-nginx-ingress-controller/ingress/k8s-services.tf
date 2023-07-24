2abf098c6b6a422ba67504185e5f3001c5195302
mamonteiro-devops <122706149+mamonteiro-devops@users.noreply.github.com>
2023-07-24 23:09:07 +0100

    Update k8s-services.tf

--- steel-eye-nginx-ingress-controller/ingress/k8s-services.tf
+++ steel-eye-nginx-ingress-controller/ingress/k8s-services.tf
@@ -1,6 +1,22 @@
-resource "kubernetes_service_v1" "service-ing" {
+resource "kubernetes_service_v1" "app1_service-ing" {
   metadata {
-    name = "service-ing"
+    name = "steel-eye-app1-service"
+  }
+  spec {
+    #The "selector" field is used to select the Pods that the Service will route traffic to.
+    #In this case, the value of the "app" label in the Deployment's selector is used.
+    selector = {
+      app = kubernetes_deployment.example_deployment_ing.spec.0.selector.0.match_labels.app
+    }
+    port {
+      port = 5678
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "app2_service-ing" {
+  metadata {
+    name = "steel-eye-app2-service"
   }
   spec {
     #The "selector" field is used to select the Pods that the Service will route traffic to.
