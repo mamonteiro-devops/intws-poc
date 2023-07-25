9cc5aeffde941da58d1120b6b57bc0007b685164
Manuel Monteiro <manuel.monteiro@kuehne-nagel.com>
2023-07-25 18:48:02 +0100

    NOISSUE

---
+++ steel-eye-nginx-ingress-controller/ingress/k8s-ingress-v2.tf
@@ -0,0 +1,58 @@
+
+resource "kubernetes_ingress_v1" "hello_kubernetes" {
+  metadata {
+    name = "hello-kubernetes"
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme"     = "internet-facing"
+      "kubernetes.io/ingress.class"          = "alb"
+    }
+  }
+  spec {
+    ingress_class_name = "nginx"
+    rule {
+      http {
+        path {
+          backend {
+            service {
+              name = "steel-eye-app1-service"
+              port {
+                number = 5678
+              }
+            }
+          }
+          path = "/app1"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "hello_kubernetes" {
+  metadata {
+    name = "hello-kubernetes"
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        name = "hello-kubernetes"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          name = "hello-kubernetes"
+        }
+      }
+
+      spec {
+        container {
+          image = "hashicorp/http-echo"
+          name  = "simple-steel-eye-app1"
+          args  = ["-text=Hello from my simple-steel-eye-app1"]
+        }
+      }
+    }
+  }
+}
